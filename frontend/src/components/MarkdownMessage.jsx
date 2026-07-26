e0305c11f03799df181bba117b2354219cf273ed
Sanu00 <sanusharma0045.v@gmail.com>
2026-07-27 01:28:49 +0530

    k

--- frontend/src/components/MarkdownMessage.jsx
+++ frontend/src/components/MarkdownMessage.jsx
@@ -3,7 +3,7 @@ import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
 
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
-import { oneDark } from "react-syntax-highlighter/dist/esm/styles/prism";
+import { oneDark } from "react-syntax-highlighter/dist/cjs/styles/prism";
 
 import { CopyToClipboard } from "react-copy-to-clipboard";
 
